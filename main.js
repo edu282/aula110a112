3817d7e97dc7ae24b85faa2d53520fe9eff0a325
edu282 <91227097+edu282@users.noreply.github.com>
2023-07-20 16:52:31 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,17 +1,34 @@
+prediction1 = ""
 
+prediction2 = ""
 
+Webcam.set
+({
+  width: 350,
+  height: 300, 
+  imageFormat:'png',
+  pngQuality: 90
+});
 
+camera = document.getElementById("camera");
+Webcam.attach('#camera');
 
+function takeSnapshot()
+{
+  Webcam.snap(function(data_uri)
+    {
+      document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
 
+    });
+}
+console.log('ml5 version:', ml5.version);
 
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/q2PDnlO6b/model.json',modelLoaded);
 
-
-
-
-
-
-
-
+function modelLoaded()
+{
+  console.log ('modelo carregado');
+}
 
 function speak(){
     var synth = window.speechSynthesis;
